75932a86480ab3c19efe6c39e432e67314671549
Jesse Geerts <jessegeerts@hotmail.nl>
2015-12-26 12:13:11 +0100

    Create Join.java

---
+++ src/net/gravitynetwork/gravitypb/listener/Join.java
@@ -0,0 +1,19 @@
+package net.gravitynetwork.gravitypb.listener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Created by jesse on 26-12-15.
+ */
+public class Join implements Listener {
+
+
+    public void onJoin(PlayerJoinEvent e){
+        // TODO: Add player to the team and customize the join message
+        // TODO: for settings the player in that team but also a message with You are in RED or BLUE
+
+
+    }
+
+}
